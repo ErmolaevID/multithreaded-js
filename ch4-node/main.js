2615e5d5d851555df59e20a8c791aca3b445358f
Ermolaev Ilya <Ermolaev.ID@yandex.ru>
2022-05-22 23:42:21 +0500

    add

---
+++ ch4-node/main.js
@@ -0,0 +1,15 @@
+const { Worker } = require('node:worker_threads');
+const worker = new Worker(__dirname + '/worker-node.js');
+
+const buffer = new SharedArrayBuffer(1024);
+const view = new Uint16Array(buffer);
+
+console.log('now', view[0]);
+
+worker.postMessage(buffer);
+
+setTimeout(() => {
+  console.log('after', view[0]);
+  console.log('prop', buffer.foo);
+  worker.unref();
+}, 500);
